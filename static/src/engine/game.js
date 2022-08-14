ee7d973c40800f5aa2278d72d51fe2eab1edefae
Samuil <samuil.pavlevchev@gmail.com>
2022-08-14 10:26:34 +0100

    Tank moves and turns!

--- static/src/engine/game.js
+++ static/src/engine/game.js
@@ -1,5 +1,11 @@
 import { createRenderer } from "./engine.js";
 
+const ACCELERATION = 600;
+const MAX_SPEED = 300;
+const TURN_RATE = 3;
+const FRICTION = 1000;
+
+
 export async function start(username, gameId) {
     const controls = {};
     // All are beginning value. x starts at 100. speed starts at 0.
@@ -19,7 +25,36 @@ export async function start(username, gameId) {
     engine.registerMain(render, tick);
 
     function tick() {
-        
+        // First do player controllers, then simulation.
+        // Movement
+        if (controls['ArrowUp']) {
+            player.speed += ACCELERATION * engine.STEP_SIZE_S;
+        } else if (controls['ArrowDown']) {
+            player.speed -= ACCELERATION * engine.STEP_SIZE_S;
+        } else if(player.speed > 0) {
+            // Add friction so it stops automatically. math.max the bigger value of the two so the tank doesnt bump backwards.
+            player.speed = Math.max(player.speed - FRICTION * engine.STEP_SIZE_S, 0);
+        } else if(player.speed < 0) {
+            player.speed = Math.min(player.speed + FRICTION * engine.STEP_SIZE_S, 0);
+        }
+
+        if (Math.abs(player.speed) > MAX_SPEED) {
+            // To prevent when going backawrds to switch the speed we multiply it by the sign = or - of player speed.
+            player.speed = MAX_SPEED * Math.sign(player.speed);
+        }
+
+        // Turning around its own axis.
+        if (controls['ArrowLeft']) {
+            player.direction -= TURN_RATE * engine.STEP_SIZE_S;
+        } else if (controls['ArrowRight']) {
+            player.direction += TURN_RATE * engine.STEP_SIZE_S
+        }
+
+        if (player.speed != 0) {
+            // cos for x and sin for y
+            player.x += Math.cos(player.direction) * player.speed * engine.STEP_SIZE_S;
+            player.y += Math.sin(player.direction) * player.speed * engine.STEP_SIZE_S;
+        }
     };
 
     function render() {
